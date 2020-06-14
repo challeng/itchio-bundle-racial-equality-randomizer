698e525cea75f8fff81a39bab808bef88fe48616
Jim Challenger <jimchallenger@MacBook-Pro.local>
2020-06-14 11:48:20 -0500

    Updates

---
+++ precache-manifest.91ba0a06b469acd7201e30ab7a5ce98a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa22d6541da252bc43a7053d421f64b",
+    "url": "/itchio-bundle-racial-equality-randomizer/index.html"
+  },
+  {
+    "revision": "d088422ea44362ce6d19",
+    "url": "/itchio-bundle-racial-equality-randomizer/static/css/main.54d1bb2a.chunk.css"
+  },
+  {
+    "revision": "8a92d44d0e5937cf83f3",
+    "url": "/itchio-bundle-racial-equality-randomizer/static/js/2.94621bc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/itchio-bundle-racial-equality-randomizer/static/js/2.94621bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d088422ea44362ce6d19",
+    "url": "/itchio-bundle-racial-equality-randomizer/static/js/main.d1335c94.chunk.js"
+  },
+  {
+    "revision": "345d321c5f454c1b61cd",
+    "url": "/itchio-bundle-racial-equality-randomizer/static/js/runtime-main.8f4eca85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/itchio-bundle-racial-equality-randomizer/static/media/logo.5d5d9eef.svg"
+  }
+]);
